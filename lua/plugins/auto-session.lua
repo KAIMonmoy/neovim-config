8e6436209e278a8b3203ba7248f29cfadcd92deb
KAIMonmoy <aftarulislam@gmail.com>
2024-09-30 04:22:59 +0600

    remove type suggestion from auto-session

--- lua/plugins/auto-session.lua
+++ lua/plugins/auto-session.lua
@@ -3,9 +3,6 @@ return {
   'rmagatti/auto-session',
   lazy = false,
 
-  ---enables autocomplete for opts
-  ---@module "auto-session"
-  ---@type AutoSession.Config
   opts = {
     suppressed_dirs = { '~/', '~/Downloads' },
   }
