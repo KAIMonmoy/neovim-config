9ed08e02b4a7183722820f79a84d1f620eb628f4
KAIMonmoy <aftarulislam@gmail.com>
2024-09-30 03:40:48 +0600

    remove theme from lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -41,7 +41,6 @@ return {
     require('lualine').setup {
       options = {
         icons_enabled = true,
-        theme = 'catpuccin',
         section_separators = { left = '', right = '' },
         component_separators = { left = '', right = '' },
         disabled_filetypes = { 'alpha', 'neo-tree' },
